3ac2676ef250294390b91d3a6f6c5b92522acd27
estet-osa <lambfilm@yandex.ru>
2015-06-26 00:58:17 +0400

    modifi index.php, delete left code

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/*
-if($_SERVER['REMOTE_ADDR'] != '46.173.3.84'){
-	header("Location: http://tatuirovki.hard-rock-tattoo.ru/");
-}*/
-
-
 set_include_path(get_include_path().PATH_SEPARATOR.'application/controllers'
 		.PATH_SEPARATOR.'application/models'
 		.PATH_SEPARATOR.'application/views');
